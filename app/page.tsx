33d2fb73fc52688e6620712acb9a0441350db82d
Julian Andrade Silva <julian-andrade@hotmail.com>
2024-04-30 19:52:44 -0300

    feat: "add first promo banner at homepage"

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,28 @@ import CategoryList from './_components/category-list'
 import Header from './_components/header'
 import Search from './_components/search'
 
+// Image
+import Image from 'next/image'
+
 const Home = () => {
   return (
     <>
       <div className="container">
         <Header />
+
         <Search />
+
         <CategoryList />
+
+        <Image
+          src={'/banner_promo_01.png'}
+          alt="banner de desconto 1"
+          width={0}
+          height={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   )
