67fe7760125d2db6e471659d413e1fdaeba30c4d
Julian Andrade Silva <julian-andrade@hotmail.com>
2024-04-30 19:51:21 -0300

    fix: "change overflow-x-scroll for overflow-x-auto"

--- app/_components/category-list.tsx
+++ app/_components/category-list.tsx
@@ -7,7 +7,7 @@ const CategoryList = async () => {
   const categories = await db.category.findMany({})
 
   return (
-    <div className="flex gap-3 overflow-x-scroll pb-6 pt-6">
+    <div className="flex gap-3 overflow-x-auto pb-6 pt-6 [&::-webkit-scrollbar]:hidden">
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
