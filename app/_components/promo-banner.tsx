c084402fc78a8b452066b443dd98e0bfceed29c9
Julian Andrade Silva <julian-andrade@hotmail.com>
2024-05-01 15:40:28 -0300

    feat: add promo-banner component

---
+++ app/_components/promo-banner.tsx
@@ -0,0 +1,17 @@
+// Next
+import Image, { ImageProps } from 'next/image'
+
+const PromoBanner = (props: ImageProps) => {
+  return (
+    <Image
+      width={0}
+      height={0}
+      className="mb-6 h-auto w-full object-contain"
+      sizes="100vw"
+      quality={100}
+      {...props}
+    />
+  )
+}
+
+export default PromoBanner
