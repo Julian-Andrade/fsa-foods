899ee1531bba7e18cadd25b91d8387f94dbd0ec9
Julian Andrade Silva <julian-andrade@hotmail.com>
2024-05-06 18:09:00 -0300

    feat: increase variant with has no button

--- app/_components/badge-title.tsx
+++ app/_components/badge-title.tsx
@@ -5,19 +5,24 @@ import { ChevronRight } from 'lucide-react'
 
 interface BadgeTitleProps {
   title?: string
+  variant?: 'button' | 'noButton'
 }
 
-const BadgeTitle = ({ title }: BadgeTitleProps) => {
+const BadgeTitle = ({ title, variant }: BadgeTitleProps) => {
   return (
     <div className="flex items-center justify-between">
       <h2 className="gap-1 font-bold">{title}</h2>
-      <Button
-        className="flex h-0 items-center p-0 text-primary hover:bg-transparent"
-        variant={'ghost'}
-      >
-        Ver todos
-        <ChevronRight size={16} />
-      </Button>
+      {variant === 'button' ? (
+        <Button
+          className="flex h-0 items-center p-0 text-primary hover:bg-transparent"
+          variant={'ghost'}
+        >
+          Ver todos
+          <ChevronRight size={16} />
+        </Button>
+      ) : (
+        ''
+      )}
     </div>
   )
 }
