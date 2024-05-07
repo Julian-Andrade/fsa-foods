e1e5135803bb57572c674fd03d726c8a008b46fa
Julian Andrade Silva <julian-andrade@hotmail.com>
2024-05-07 10:50:41 -0300

    refactor: add className prop to component

--- app/_components/badge-title.tsx
+++ app/_components/badge-title.tsx
@@ -1,25 +1,40 @@
+// Next
+import Link from 'next/link'
 // Components
 import { Button } from './ui/button'
 // Icon
 import { ChevronRight } from 'lucide-react'
+// Utils
+import { cn } from '../_lib/utils'
 
 interface BadgeTitleProps {
   title?: string
   variant?: 'button' | 'noButton'
+  href: string
+  nameButton?: string
+  className?: string
 }
 
-const BadgeTitle = ({ title, variant }: BadgeTitleProps) => {
+const BadgeTitle = ({
+  title,
+  variant,
+  href,
+  nameButton,
+  className,
+}: BadgeTitleProps) => {
   return (
-    <div className="flex items-center justify-between">
+    <div className={cn('flex items-center justify-between', className)}>
       <h2 className="gap-1 font-bold">{title}</h2>
       {variant === 'button' ? (
-        <Button
-          className="flex h-0 items-center p-0 text-primary hover:bg-transparent"
-          variant={'ghost'}
-        >
-          Ver todos
-          <ChevronRight size={16} />
-        </Button>
+        <Link href={href}>
+          <Button
+            className="flex h-0 items-center p-0 text-primary hover:bg-transparent"
+            variant={'ghost'}
+          >
+            {nameButton}
+            <ChevronRight size={16} />
+          </Button>
+        </Link>
       ) : (
         ''
       )}
