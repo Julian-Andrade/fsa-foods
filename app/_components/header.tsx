8c62bf2fb28f6fce9a864dd3af655cf28ffa7982
Julian Andrade Silva <julian-andrade@hotmail.com>
2024-05-23 10:48:55 -0300

    feat: add initial menu sheet

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,23 +1,145 @@
+'use client'
+
 // Next
 import Image from 'next/image'
 import Link from 'next/link'
+import { signIn, signOut, useSession } from 'next-auth/react'
 // Components
-import { MenuIcon } from 'lucide-react'
+import {
+  HeartIcon,
+  HomeIcon,
+  LogOutIcon,
+  MenuIcon,
+  ScrollTextIcon,
+} from 'lucide-react'
 import { Button } from './ui/button'
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from './ui/sheet'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+import { Separator } from './ui/separator'
 
 const Header = () => {
+  const { data, status } = useSession()
+
+  const handleSignIn = () => {
+    signIn()
+  }
+
+  const handleSignOut = () => {
+    signOut()
+  }
+
   return (
     <div className="flex justify-between pt-6">
       <Link href="/">
         <Image src="/logo.png" alt="FSA Foods" height={30} width={120} />
       </Link>
-      <Button
-        size="icon"
-        variant="outline"
-        className="border-none bg-transparent"
-      >
-        <MenuIcon />
-      </Button>
+
+      <Sheet>
+        <SheetTrigger>
+          <Button
+            size="icon"
+            variant="outline"
+            className="border-none bg-transparent"
+          >
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle className="text-left">Menu</SheetTitle>
+          </SheetHeader>
+
+          {status === 'authenticated' ? (
+            <>
+              <div className="flex items-center justify-between">
+                <div className="flex items-center gap-3 py-6">
+                  <Avatar>
+                    <AvatarImage
+                      src={data?.user?.image as string | undefined}
+                    />
+                    <AvatarFallback>
+                      {data?.user?.name?.split(' ')[0][0]}
+                      {data?.user?.name?.split(' ')[1][0]}
+                    </AvatarFallback>
+                  </Avatar>
+
+                  <div>
+                    <h3 className="font-semibold">{data?.user?.name}</h3>
+                    <span className="block text-xs text-muted-foreground">
+                      {data?.user?.email}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="flex items-center justify-between py-6">
+                <h2 className="font-semibold">Olá. Faça seu login!</h2>
+                <Button onClick={handleSignIn}>Login</Button>
+              </div>
+            </>
+          )}
+
+          <div className="py-6">
+            <Separator />
+          </div>
+
+          <div className="space-y-3">
+            <Button
+              variant="ghost"
+              className="w-full justify-start space-x-3 rounded-full"
+            >
+              <HomeIcon size={16} />
+              <span className="block">Início</span>
+            </Button>
+
+            {data?.user && (
+              <>
+                <Button
+                  variant="ghost"
+                  className="w-full justify-start space-x-3 rounded-full"
+                >
+                  <ScrollTextIcon size={16} />
+                  <span className="block">Meus Pedidos</span>
+                </Button>
+
+                <Button
+                  variant="ghost"
+                  className="w-full justify-start space-x-3 rounded-full"
+                >
+                  <HeartIcon size={16} />
+                  <span className="block">Restaurantes Favoritos</span>
+                </Button>
+              </>
+            )}
+          </div>
+
+          <div className="py-6">
+            <Separator />
+          </div>
+
+          {data?.user && (
+            <>
+              <Button
+                variant="ghost"
+                className="w-full justify-start space-x-3 rounded-full"
+                onClick={handleSignOut}
+              >
+                <LogOutIcon size={16} />
+                <span className="block">Sair da conta</span>
+              </Button>
+            </>
+          )}
+        </SheetContent>
+      </Sheet>
     </div>
   )
 }
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -10,8 +10,8 @@ const handler = NextAuth({
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRECT as string,
+      clientId: process.env.AUTH_GOOGLE_ID as string,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
     }),
   ],
 })
